fd745b6a731a70ade680e08f1c2d16c4ff8d1384
simonvea <Simon@Simons-Air-2.lan>
2015-06-30 19:17:33 +0200

    Part 4 (I3) adding tags complete

--- blogger/config/routes.rb
+++ blogger/config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
     
+    root 'articles#index'
+    
     resources :articles do
         resources :comments
     end
     
-    root 'articles#index'
+    resources :tags
+    
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
